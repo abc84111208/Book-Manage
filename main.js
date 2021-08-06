3b70a53dbc93bc127e813025c5a9f9122db07052
闕瑋霆 <abc84111208@gmail.com>
2021-08-06 22:34:16 +0800

    reserve 39be308 commit

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 require('@electron/remote/main').initialize()
+
+const template = [
+    {
+        label: '一般',
+        submenu: [
+            { label: '關於', role: 'about' },
+            { type: 'separator' },
+            { label: '離開', role: 'quit' }
+        ]
+    },
+    {
+        label: '編輯',
+        submenu: [
+            { label: '復原', role: 'undo' },
+            { label: '取消復原', role: 'redo' },
+            { type: 'separator' },
+            { label: '剪下', role: 'cut' },
+            { label: '複製', role: 'copy' },
+            { label: '貼上', role: 'paste' },
+            { label: '刪除', role: 'delete' }
+
+        ]
+    },
+    {
+        label: '視窗',
+        submenu: [
+            { label: '重新載入', role: 'reload' },
+            { label: '除錯工具', role: 'toggleDevTools' },
+            { type: 'separator' },
+            { label: '放大', role: 'zoomIn' },
+            { label: '縮小', role: 'zoomOut' },
+            { label: '重置大小', role: 'resetZoom' }
+        ]
+    },
+]
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -13,6 +48,8 @@ function createWindow() {
             preload: path.join(__dirname, 'preload.js')
         }
     })
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(null);
     win.loadFile('index.html')
     win.webContents.openDevTools()
 }
